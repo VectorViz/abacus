85c8cad06d84004008205baad08abeb16d469620
Nathan Brown <32497422+KingOfTac@users.noreply.github.com>
2025-09-20 12:59:08 -0600

    exploring using rust for entire web app

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use web_sys::window;
+
+fn main() {
+  console_error_panic_hook::set_once();
+
+  let document = window()
+    .and_then(|win| win.document())
+    .expect("Could not access the document");
+  let body = document.body().expect("Could not access document.body");
+  let text_node = document.create_text_node("Hello world!");
+
+  body.append_child(text_node.as_ref())
+    .expect("Could not append text node to body");
+}
